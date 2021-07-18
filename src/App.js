23f6b89b4518ef89231be7c94271594cb264d361
danial <Daniiiaal.ha@gmail.com>
2021-07-19 01:24:54 +0430

    add loginScreen

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MainScreen from "./screens/MainScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import UserProfileScreen from "./screens/UserProfileScreen";
 import TabMenu from "./components/TabMenu";
+import LoginScreen from "./screens/LoginScreen";
 
 
 function App() {
@@ -15,7 +16,8 @@ function App() {
             <Nav className="app__header"/>
             {/*<MainScreen className="app__content"/>*/}
             {/*<RegisterScreen className="app__content"/>*/}
-            <UserProfileScreen className="app__content"/>
+            <LoginScreen className="app__content"/>
+            {/*<UserProfileScreen className="app__content"/>*/}
             <Footer className="app__footer"/>
         </div>
     );
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,12 +1,42 @@
 import React from 'react';
 
 import "../assets/css/screens/loginScreenStyle.css";
+import TextInput from "../components/TextInput";
 
 
 function LoginScreen(props) {
-    return (
-        <div>
+    const fields = [
+        {
+            name: "ایمیل",
+            id: "email",
+            type: "email",
+            required: true
+        },
+        {
+            name: "رمز عبور",
+            id: "password",
+            type: "password",
+            pattern: ".{6,}",
+            required: true,
+        }
+    ]
 
+    return (
+        <div className={"loginScreen " + props.className}>
+            <h2 className="registerScreen__title">
+                فروشگاه - ورود
+            </h2>
+
+            <form className="loginScreen__form" action="/">
+                {fields.map((field, index) => (
+                        <TextInput inputProps={field}/>
+                    )
+                )}
+            </form>
+
+            <button className="loginScreen__submitButton">
+                ورود
+            </button>
         </div>
     );
 }
